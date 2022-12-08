a054f7981700deaf548d468add6889bbdf18d97d
GTech9971 <98648140+GTech9971@users.noreply.github.com>
2022-12-09 02:12:48 +0900

    tfjsのセットアップ完了 tfjs-nodeは使い物にならないので注意

--- tfjs-react-app/src/App.tsx
+++ tfjs-react-app/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import * as tf from '@tensorflow/tfjs';
+
+
 
 
 export const App = () => {
+  const dataArray = [8, 6, 8, 7, 4, 1];
+  const first = tf.tensor(dataArray);
+  console.log(first)
   return (
     <></>
   );
