52825a459d08b67a8b21b57410dc4eed9e0a3910
DJ <georgetech1984@gmail.com>
2022-12-10 00:56:34 +0900

    show tensor,png image to canvas

--- tfjs-react-app/src/App.tsx
+++ tfjs-react-app/src/App.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
 
-
-
-
 export const App = () => {
-  const dataArray = [8, 6, 8, 7, 4, 1];
-  const first = tf.tensor(dataArray);
-  console.log(first)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasImgRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const tensorPic: tf.Tensor3D = tf.randomUniform([300, 400, 3]);
+      await tf.browser.toPixels(tensorPic, canvasRef.current as HTMLCanvasElement);
+      tensorPic.dispose();
+    })()
+
+  }, [canvasRef]);
+
+
+  useEffect(() => {
+    (async () => {
+      const img = new Image();
+      img.src = "../ap2n-header.png";
+      img.onload = async () => {
+        console.log("A");
+        const t: tf.Tensor3D = await tf.browser.fromPixelsAsync(img);
+        await tf.browser.toPixels(t, canvasImgRef.current as HTMLCanvasElement);
+        t.dispose();
+      }
+    })();
+
+  }, [canvasImgRef]);
+
   return (
-    <></>
+    <>
+      <canvas ref={canvasRef}></canvas>
+      <canvas ref={canvasImgRef}></canvas>
+    </>
   );
 }
